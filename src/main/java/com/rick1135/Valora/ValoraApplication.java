5deba6dbd59341f0e7bbe49c911c7c6f2dfaab0f
rick1135 <rick1135@hotmail.com>
2026-03-18 09:02:21 -0300

    commit inicial

---
+++ src/main/java/com/rick1135/Valora/ValoraApplication.java
@@ -0,0 +1,13 @@
+package com.rick1135.Valora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rick1135/Valora/ValoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick1135.Valora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
