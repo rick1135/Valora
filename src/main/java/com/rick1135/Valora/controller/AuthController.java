15df3c8c56e3c27b4e90b3667795a21968ffef21
rick1135 <rick1135@hotmail.com>
2026-03-18 20:14:04 -0300

    feat: refatorando AuthController

--- src/main/java/com/rick1135/Valora/controller/AuthController.java
+++ src/main/java/com/rick1135/Valora/controller/AuthController.java
@@ -8,11 +8,11 @@ import com.rick1135.Valora.entity.UserRole;
 import com.rick1135.Valora.exception.EmailAlreadyRegisteredException;
 import com.rick1135.Valora.repository.UserRepository;
 import com.rick1135.Valora.service.TokenService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,15 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @Autowired
-    TokenService tokenService;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(
+            TokenService tokenService,
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO data) {
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody AuthenticationDTO data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = authenticationManager.authenticate(usernamePassword);
         var token = tokenService.generateToken((User) auth.getPrincipal());
@@ -37,11 +47,11 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<LoginResponseDTO> register(@RequestBody RegisterDTO data) {
+    public ResponseEntity<LoginResponseDTO> register(@Valid @RequestBody RegisterDTO data) {
         if(userRepository.findByEmail(data.email()).isPresent()){
             throw new EmailAlreadyRegisteredException("Email ja cadastrado.");
         }
-        String password = new BCryptPasswordEncoder().encode(data.password());
+        String password = passwordEncoder.encode(data.password());
         User newUser = new User();
         newUser.setEmail(data.email());
         newUser.setName(data.name());
